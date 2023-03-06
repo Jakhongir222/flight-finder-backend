fa8d7c430f97d6f4e0aa922f2ea76418c3c79dd1
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-03-06 11:40:11 +0100

    Initial commit

---
+++ src/main/java/com/example/flightfinderbackend/FlightFinderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.flightfinderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightFinderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightFinderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flightfinderbackend/FlightFinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flightfinderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightFinderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
